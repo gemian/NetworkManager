dc1cbb194b15858457591cde2702815aa7ed4b55
Lubomir Rintel <lkundrak@v3.sk>
2018-05-15 16:25:29 +0200

    platform: don't initialize pllink when not needed

--- src/platform/nm-linux-platform.c
+++ src/platform/nm-linux-platform.c
@@ -5988,9 +5988,9 @@ wifi_get_wifi_data (NMPlatform *platform, int ifindex)
 	WifiData *wifi_data;
 
 	wifi_data = g_hash_table_lookup (priv->wifi_data, GINT_TO_POINTER (ifindex));
-	pllink = nm_platform_link_get (platform, ifindex);
 
 	if (!wifi_data) {
+		pllink = nm_platform_link_get (platform, ifindex);
 		if (pllink) {
 			if (pllink->type == NM_LINK_TYPE_WIFI)
 				wifi_data = wifi_utils_init (ifindex, TRUE);
